6e8fc68d928d025948adbae98d76c9bedcdbf521
Harini Anand <harinianandbsc24@rvu.edu.in>
2026-09-07 19:46:56 +0530

    Update Campus Connect portal

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+
+// Mounts React specifically to your auth-root div in index.html
+createRoot(document.getElementById('auth-root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
